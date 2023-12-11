b952384a0ff94dcc5816d2d855d8f82917ecb915
yumaojun <18108053819@163.com>
2023-12-11 16:41:24 +0800

    补充测试

--- apps/event/impl/impl_test.go
+++ apps/event/impl/impl_test.go
@@ -1,18 +1,79 @@
 package impl_test
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
 	"testing"
 
 	"github.com/infraboard/maudit/apps/event"
+	"github.com/segmentio/kafka-go"
 )
 
-var (
-	svc event.ServiceServer
-)
+func TestPublishEvent(t *testing.T) {
+	w := &kafka.Writer{
+		Addr:                   kafka.TCP("localhost:9092"),
+		Topic:                  "audit_event",
+		Balancer:               &kafka.LeastBytes{},
+		AllowAutoTopicCreation: true,
+	}
 
-func TestSaveEvent(t *testing.T) {
+	data := &event.OperateEventData{
+		UserName:     "张三",
+		ServiceName:  "cmdb",
+		ResourceType: "secret",
+		Action:       "create",
+		Request:      "{}",
+		Response:     "{}",
+	}
+	e, err := event.NewJsonOperateEvent(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	payload, err := json.Marshal(e)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	err = w.WriteMessages(context.Background(),
+		kafka.Message{
+			Value: payload,
+		},
+	)
+
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := w.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
 }
 
-func init() {
+func TestConsumerEvent(t *testing.T) {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  []string{"localhost:9092"},
+		GroupID:  "consumer-group-id",
+		Topic:    "audit_event",
+		MaxBytes: 10e6, // 10MB,
+	})
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			break
+		}
+		fmt.Printf("message at topic/partition/offset %v/%v/%v: %s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
+
+		// 处理完消息后需要提交该消息已经消费完成, 消费者挂掉后保存消息消费的状态
+		// FetchMessage() / CommitMessages(ctx, m) 分段提交
+		// if err := r.CommitMessages(ctx, m); err != nil {
+		//     log.Fatal("failed to commit messages:", err)
+		// }
+	}
+
+	if err := r.Close(); err != nil {
+		log.Fatal("failed to close reader:", err)
+	}
 }
