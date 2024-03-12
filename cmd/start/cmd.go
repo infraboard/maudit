7f8e7e9376c118b203a8fc7dd8fc939d773f3610
yumaojun <18108053819@163.com>
2024-03-12 11:23:31 +0800

    更新mcube

--- cmd/start/cmd.go
+++ cmd/start/cmd.go
@@ -5,11 +5,11 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/infraboard/mcenter/clients/rpc/hooks"
 	"github.com/infraboard/mcube/v2/ioc/server"
 
 	// 注册所有服务
 	_ "github.com/infraboard/maudit/apps"
+	_ "github.com/infraboard/mcenter/clients/rpc/middleware"
 )
 
 // Cmd represents the start command
@@ -18,7 +18,6 @@ var Cmd = &cobra.Command{
 	Short: "maudit API服务",
 	Long:  "maudit API服务",
 	Run: func(cmd *cobra.Command, args []string) {
-		hooks.NewMcenterAppHook().SetupAppHook()
 		cobra.CheckErr(server.Run(context.Background()))
 	},
 }
